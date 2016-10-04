8ecefa1abfe6448056d8826630eb3811b5474483
Brett Lamy <blamy@calvauto.com>
2016-10-04 07:13:31 -0400

    2 more files

--- test/CreateDDLMySQLTest.java
+++ test/CreateDDLMySQLTest.java
@@ -1,4 +1,5 @@
 import junit.framework.TestCase;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,99 @@ import static org.junit.Assert.*;
  * Created by brett on 9/30/16.
  */
 public class CreateDDLMySQLTest extends TestCase {
+    CreateDDLMySQL fixture;
+
+    // Field Types
+    private final int VARCHAR = 0;
+    private final int BOOL = 1;
+    private final int INT = 2;
+    private final int DOUBLE = 3;
+
+
 
     @Test
     public void testCreateDDL() throws Exception {
+        EdgeTable usersTable = new EdgeTable("0|users");
+        usersTable.addRelatedTable(1);
+        usersTable.addNativeField(0);
+        usersTable.addNativeField(1);
+        usersTable.addNativeField(2);
+        usersTable.makeArrays();
+//        usersTable.setRelatedField(0, 4);
+//        usersTable.setRelatedField(0, 5);
 
+        EdgeField userId = new EdgeField("0|ID");
+        userId.setTableID(0);
+//        userId.setTableBound(8);
+//        userId.setFieldBound(8);
+        userId.setDisallowNull(true);
+        userId.setIsPrimaryKey(true);
+        userId.setDataType(INT);
+
+        EdgeField username = new EdgeField("1|username");
+        username.setTableID(0);
+//        username.setTableBound(8);
+//        username.setFieldBound(8);
+        username.setDisallowNull(true);
+        username.setVarcharValue(255);
+        username.setDataType(VARCHAR);
+
+        EdgeField password = new EdgeField("2|password");
+        password.setTableID(0);
+//        password.setTableBound(8);
+//        password.setFieldBound(8);
+        password.setDisallowNull(true);
+        password.setVarcharValue(255);
+        password.setDataType(VARCHAR);
+
+        EdgeTable messagesTable = new EdgeTable("1|messages");
+        messagesTable.addRelatedTable(0);
+        messagesTable.addNativeField(3);
+        messagesTable.addNativeField(4);
+        messagesTable.addNativeField(5);
+        messagesTable.addNativeField(6);
+        messagesTable.makeArrays();
+//        messagesTable.setRelatedField(4, 0);
+//        messagesTable.setRelatedField(5, 0);
+
+        EdgeField messageId = new EdgeField("3|ID");
+        messageId.setTableID(1);
+//        messageId.setTableBound(8);
+//        messageId.setFieldBound(8);
+        messageId.setDisallowNull(true);
+        messageId.setIsPrimaryKey(true);
+        messageId.setDataType(INT);
+
+        EdgeField toUser = new EdgeField("4|toUser");
+        messageId.setTableID(1);
+//        messageId.setTableBound(0);
+        messageId.setDisallowNull(true);
+        messageId.setDataType(INT);
+
+        EdgeField fromUser = new EdgeField("5|fromUser");
+        messageId.setTableID(1);
+//        messageId.setTableBound(0);
+        messageId.setDisallowNull(true);
+        messageId.setDataType(BOOL);
+
+        EdgeField message = new EdgeField("6|message");
+        message.setTableID(1);
+        message.setDisallowNull(true);
+        message.setDefaultValue("");
+        message.setVarcharValue(255);
+        message.setDataType(VARCHAR);
+
+        fixture = new CreateDDLMySQL(
+                new EdgeTable[] { usersTable, messagesTable },
+                new EdgeField[] {
+                        userId, username, password,
+                        messageId, toUser, fromUser, message
+                }
+        );
+
+//        fixture.createDDL();
+
+        System.out.println(fixture.getSQLString());
     }
 
     @Test
--- test/EdgeTableTest.java
+++ test/EdgeTableTest.java
@@ -11,62 +11,84 @@ public class EdgeTableTest extends TestCase {
     @Test
     public void testConstructor() throws Exception {
         EdgeTable edgeTable = new EdgeTable("1|brett");
-        assertEquals("Should parse number figure", edgeTable.getNumFigure(), 1);
-        assertEquals("Should Parse name", edgeTable.getName(), "brett");
+        assertEquals("Should parse number figure", 1, edgeTable.getNumFigure());
+        assertEquals("Should Parse name", "brett", edgeTable.getName());
     }
 
     @Test
-    public void testGetName() throws Exception {
-
+    public void testRelatedTable() throws Exception {
+        EdgeTable edgeTable = new EdgeTable("1|brett");
+        edgeTable.addRelatedTable(0);
+        edgeTable.addRelatedTable(1);
+        edgeTable.addRelatedTable(2);
+        edgeTable.makeArrays();
+        assertEquals("Should properly set testRelatedTable[1]", 0, edgeTable.getRelatedTablesArray()[0]);
+        assertEquals("Should properly set testRelatedTable[1]", 1, edgeTable.getRelatedTablesArray()[1]);
+        assertEquals("Should properly set testRelatedTable[1]", 2, edgeTable.getRelatedTablesArray()[2]);
     }
 
     @Test
-    public void testAddRelatedTable() throws Exception {
-
-    }
-
-    @Test
-    public void testGetRelatedTablesArray() throws Exception {
-
-    }
-
-    @Test
-    public void testGetRelatedFieldsArray() throws Exception {
-
-    }
-
-    @Test
-    public void testSetRelatedField() throws Exception {
-
-    }
-
-    @Test
-    public void testGetNativeFieldsArray() throws Exception {
-
-    }
-
-    @Test
-    public void testAddNativeField() throws Exception {
-
+    public void testNativeFieldsArray() throws Exception {
+        EdgeTable edgeTable = new EdgeTable("1|brett");
+        edgeTable.addNativeField(0);
+        edgeTable.addNativeField(1);
+        edgeTable.addNativeField(2);
+        edgeTable.makeArrays();
+        assertEquals("Should properly set setGetRelatedFields[1]", 0, edgeTable.getNativeFieldsArray()[0]);
+        assertEquals("Should properly set setGetRelatedFields[1]", 1, edgeTable.getNativeFieldsArray()[1]);
+        assertEquals("Should properly set setGetRelatedFields[1]", 2, edgeTable.getNativeFieldsArray()[2]);
     }
 
     @Test
     public void testMoveFieldUp() throws Exception {
-
+        EdgeTable edgeTable = new EdgeTable("1|brett");
+        edgeTable.addNativeField(0);
+        edgeTable.addNativeField(1);
+        edgeTable.addNativeField(2);
+        edgeTable.makeArrays();
+        edgeTable.setRelatedField(1, 1);
+        edgeTable.setRelatedField(2, 2);
+        edgeTable.moveFieldUp(2);
+        assertEquals("Should properly set setGetRelatedFields[1]", 0, edgeTable.getRelatedFieldsArray()[0]);
+        assertEquals("Should properly set setGetRelatedFields[1]", 2, edgeTable.getRelatedFieldsArray()[1]);
+        assertEquals("Should properly set setGetRelatedFields[1]", 1, edgeTable.getRelatedFieldsArray()[2]);
     }
 
     @Test
     public void testMoveFieldDown() throws Exception {
-
-    }
-
-    @Test
-    public void testMakeArrays() throws Exception {
-
+        EdgeTable edgeTable = new EdgeTable("1|brett");
+        edgeTable.addNativeField(0);
+        edgeTable.addNativeField(1);
+        edgeTable.addNativeField(2);
+        edgeTable.makeArrays();
+        edgeTable.setRelatedField(1, 1);
+        edgeTable.setRelatedField(2, 2);
+        edgeTable.moveFieldDown(1);
+        assertEquals("Should properly set setGetRelatedFields[1]", 0, edgeTable.getRelatedFieldsArray()[0]);
+        assertEquals("Should properly set setGetRelatedFields[1]", 2, edgeTable.getRelatedFieldsArray()[1]);
+        assertEquals("Should properly set setGetRelatedFields[1]", 1, edgeTable.getRelatedFieldsArray()[2]);
     }
 
     @Test
     public void testToString() throws Exception {
+        EdgeTable edgeTable = new EdgeTable("1|brett");
+        edgeTable.addRelatedTable(1);
+        edgeTable.addRelatedTable(2);
+        edgeTable.addNativeField(0);
+        edgeTable.addNativeField(1);
+        edgeTable.addNativeField(2);
+        edgeTable.makeArrays();
+        String expected = (
+            "Table:1" +
+                "{" +
+                    "TableName: brett" +
+                    "NativeFields: 0|1|2" +
+                    "RelatedTables: 1|2" +
+                    "RelatedFields: 0|0|0" +
+                "}"
+        ).replaceAll("\\s","");
 
+        String actual = edgeTable.toString().replaceAll("\\s","");
+        assertEquals("", actual, expected);
     }
 }
